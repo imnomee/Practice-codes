67c7243736c5f42401ca9d8b792de8a2518e4226
Noman Rafiq <59981447+imnomee@users.noreply.github.com>
2020-01-22 21:22:25 +0000

    Update arrow_functions.js
    added multiple short syntax and samples

--- arrow_functions.js
+++ arrow_functions.js
@@ -35,13 +35,18 @@ function cube(x) {
 
 
 //Arrow Syntax
-cosnt square = (x)=> {
+const square = (x)=> {
     return x * x;
 }
+//OR more concise 
+const squuare = x => x * x;
+//----------------
 
 const cube = (x) => {
     return square(x) * x;
 }
+// OR
+const cube = x => square(x) * x;
 
 
 //03 MULTIPLE ARGUMENTS
@@ -62,11 +67,41 @@ function subtract(a, b) {
 const multiply = (x, y) => {
     return x * y;
 }
-
+//OR
+const multiply = (x , y) => x * y;
+//------------------
 const add = (a, b) => {
     return a + b;
 }
-
+//OR
+const add = (a, b) => a + b;
+//------------
 const subtract = (a, b) => {
     return a - b;
 }
+//OR
+const subtract = (a, b) => a - b;
+
+// 04 SAME FUNCTIONS
+//Function Declaration
+
+function divide1(a, b) {
+    return a / b; 
+  }
+  
+  //Function Expression
+  
+  const divide2 = function(a, b) {
+    return a / b;
+  }
+  
+  //Arrow Function Expression
+  
+  const divide3 = (a, b) => {
+    return a / b;
+  }
+  
+  //Arrow Function Expression - concise
+  
+  const divide4 = (a, b) => a / b;
+  
