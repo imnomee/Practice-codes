f01826c96fa5297d2a93694d07657c0a3d361384
Noman Rafiq <59981447+imnomee@users.noreply.github.com>
2020-01-22 14:53:44 +0000

    Create stringLiterals.js

---
+++ stringLiterals.js
@@ -0,0 +1,22 @@
+
+//Normal Syntax
+function like(thing) {
+    return 'I like ' + thing;
+  }
+  
+  //String Literal Interpolation
+  function love(thing) {
+    return `I love ${thing}`;
+  }
+  
+  //normal sentence
+  //const sentence = `<p>` + like(`apples`) + `.</p>`;
+  
+  //Interpolated sentence
+  const sentence = `<p>${like('apples')} but ${love('Mangoes')}.</p>`;
+  document.querySelector('.interpolation').innerHTML = sentence;
+  
+
+  /*MDN link
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+  */
