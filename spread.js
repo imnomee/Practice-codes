6a56fcd4e2df5efb0b6e9adb58376d61619d2db6
Noman Rafiq <59981447+imnomee@users.noreply.github.com>
2020-01-22 13:45:25 +0000

    Create spread.js

---
+++ spread.js
@@ -0,0 +1,53 @@
+
+
+const teachers = [
+    {
+        name: 'Ashley',
+        topicArea: 'Javascript'
+    }
+];
+
+const courses = ['Introducing JavaScript',
+    'JavaScript Basics',
+    'JavaScript Loops, Arrays and Objects',
+    'Getting Started with ES2015',
+    'JavaScript and the DOM',
+    'DOM Scripting By Example'];
+
+var i = courses.length;
+
+function addNewTeachers(newTeachers) {
+    teachers.push(...newTeachers);
+
+}
+
+function printTreehouseSummary() {
+
+    for (let i = 0; i < teachers.length; i++) {
+        console.log(`${teachers[i].name} teaches ${teachers[i].topicArea}`);
+    }
+
+    console.log(`Treehouse has ${i} JavaScript courses, and ${teachers.length} Javascript teachers`);
+}
+
+let newTeachers = [
+    {
+        name: 'James',
+        topicArea: 'Javascript'
+    },
+    {
+        name: 'Treasure',
+        topicArea: 'Javascript'
+    },
+];
+
+addNewTeachers(newTeachers);
+printTreehouseSummary();
+
+
+
+
+
+
+
+
