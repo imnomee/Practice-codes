1b6e35c8ad5717c2c1629f71719a38eeba7cfd7d
Noman Rafiq <59981447+imnomee@users.noreply.github.com>
2020-01-22 22:54:56 +0000

    formatted code
    code formatting

--- arrow_functions.js
+++ arrow_functions.js
@@ -127,15 +127,15 @@ const printToTen = () => {
 
 const addUs = (num1, num2) => console.log(num1 + num2);
 
-const multiplyUs = (num1, num2) => console.log(num1 * num2); 
+const multiplyUs = (num1, num2) => console.log(num1 * num2);
 
 const subtractFromTen = num => {
-	const diff = 10 - num; 
-	console.log(diff);
+    const diff = 10 - num;
+    console.log(diff);
 }
 
 const printDate = date => console.log(date);
 
-multiplyUs(2,6);
+multiplyUs(2, 6);
 subtractFromTen(4);
 printDate('1/16/18');
--- stringLiterals.js
+++ stringLiterals.js
@@ -2,21 +2,21 @@
 //Normal Syntax
 function like(thing) {
     return 'I like ' + thing;
-  }
-  
-  //String Literal Interpolation
-  function love(thing) {
-    return `I love ${thing}`;
-  }
-  
-  //normal sentence
-  //const sentence = `<p>` + like(`apples`) + `.</p>`;
-  
-  //Interpolated sentence
-  const sentence = `<p>${like('apples')} but ${love('Mangoes')}.</p>`;
-  console.log(sentence);
-  
+}
 
-  /*MDN link
-  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-  */
+//String Literal Interpolation
+function love(thing) {
+    return `I love ${thing}`;
+}
+
+//normal sentence
+//const sentence = `<p>` + like(`apples`) + `.</p>`;
+
+//Interpolated sentence
+const sentence = `<p>${like('apples')} but ${love('Mangoes')}.</p>`;
+console.log(sentence);
+
+
+/*MDN link
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+*/
